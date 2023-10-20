90ffdfcf72c47d1fc0d685e015eb51eb34dd4d05
Shantanu050 <shantanuwadgaonkar@yahoo.com>
2023-10-20 10:52:57 +0000

    this is movie

---
+++ DataLayer/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Entity;
+namespace DataLayer;
+public class DataAccess
+{
+      public static List<Movie>movieList=new List<Movie>()
+      {
+        new Movie{Id=1,Name="Toofan",Ryear=2023,Ratings=1},
+        new Movie{Id=1,Name="Bahubali",Ryear=2022,Ratings=2},
+        new Movie{Id=1,Name="Singham",Ryear=2021,Ratings=3},
+        new Movie{Id=1,Name="Ludo",Ryear=2020,Ratings=4},
+        new Movie{Id=1,Name="KGF",Ryear=2023,Ratings=5},
+        
+      };
+      public List<Movie> GetMovies()
+      {
+        return movieList;
+      }
+}
---
+++ Hello/Program.cs
@@ -0,0 +1,13 @@
+﻿using Entity;
+using BzLayer;
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+MoviesBz movie=new MoviesBz();
+List<Movie>movielist=movie.GetMovies();
+if(movielist!=null)
+{
+    foreach(var i in movielist)
+    Console.WriteLine($"{i.Id} {i.Name} {i.Ratings} {i.Ryear}");
+}
+else
+Console.WriteLine("No movies present");
