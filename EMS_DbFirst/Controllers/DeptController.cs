8c55dfb479251afd803ddd74287996c04a33197f
Iamneo <techadmin@iamneo.ai>
2023-10-30 06:54:47 +0000

    Backup Commit

--- EMS_DbFirst/Controllers/DeptController.cs
+++ EMS_DbFirst/Controllers/DeptController.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
-
+using EMS_DbFirst.Models;
 namespace EMS_DbFirst.Controllers
 {
     public class DeptController:Controller
     {
+       private readonly EmsdbContext context;
+       public DeptController(EmsdbContext _context)
+       {
+          context=_context;
+       }
         public IActionResult List()
         {
+            var data=context.Departments
             return View();
         }
     }
--- EMS_DbFirst/Models/Department.cs
+++ EMS_DbFirst/Models/Department.cs
@@ -6,9 +6,12 @@ namespace EMS_DbFirst.Models;
 public partial class Department
 {
     public int Id { get; set; }
+[Display(Name ="Department Name")]
+[Required(ErrorMessage ="Department Name cannot be blank")]
 
     public string DeptName { get; set; } = null!;
-
+[Display(Name="City")]
+[StringLength(25,MinimumLength =3,ErrorMessage ="City must be 3 char")]
     public string Location { get; set; } = null!;
 
     public virtual ICollection<Employee> Employees { get; set; } = new List<Employee>();
