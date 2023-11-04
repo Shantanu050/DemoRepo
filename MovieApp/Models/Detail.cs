fe50008799ec3de8be0cbef276abd531b1e7e933
Iamneo <techadmin@iamneo.ai>
2023-11-04 05:23:49 +0000

    Backup Commit

--- MovieApp/Models/Detail.cs
+++ MovieApp/Models/Detail.cs
@@ -1,9 +1,22 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 namespace MovieApp.Models
 {
     public class Detail
     {
-         public int DetailId{get;set;}
+        [Key]
+        public int DetailId{get;set;}
+        [Required]
+        [MaxLength(30)]
+        public string Actor{get;set;}
+        [ForeignKey("Movie")]
+        public int MovieId{get;set;}
+        [MaxLength(10)]
+        public string Gender{get;set;}
+        public string Role{get;set;}
+        public Movie Movie{get;set;}
     }
 }
