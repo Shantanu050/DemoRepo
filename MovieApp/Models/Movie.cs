2e2e7936d93e3cf607dedcbd65c56c2c30c367c1
Iamneo <techadmin@iamneo.ai>
2023-11-04 05:08:48 +0000

    Backup Commit

--- MovieApp/Models/Movie.cs
+++ MovieApp/Models/Movie.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 namespace MovieApp.Models
 {
     public class Movie
     {
-        
+        [Key]
+        public int Id{get;set;}
+        [Required]
+        public string Name{get;set;}
+        public int YearRelease{get;set;}
+        public int Rating{get;set;}
+        public Detail Detail{get;set;}
     }
 }
