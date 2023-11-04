5618b1230e5de8626148476df34d76de3c512475
Iamneo <techadmin@iamneo.ai>
2023-11-04 07:08:49 +0000

    Backup Commit

--- MovieApp/Controllers/MovieController.cs
+++ MovieApp/Controllers/MovieController.cs
@@ -2,6 +2,8 @@ using MovieApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.SqlServer;
+using System.Linq;
+using System;
 //using Swashbuckle.AspNetCore;
 
 namespace MovieApp.Controllers
@@ -26,9 +28,34 @@ namespace MovieApp.Controllers
         public IActionResult Get(int id)
         {
             if(id==null)
+            {
             return BadRequest("Id cannot be null");
-            var data=from m in context.Movies where m.Id==id select m;
+            }
+            var data=(from m in context.Movies where m.Id==id select m).FirstOrDefault();
+            if(data==null)
+            {
+                return NotFound($"Movie {id} not found");
+            }
             return Ok(data);
         }
+
+        [HttpPost]
+        [Route("AddMovie")]
+        public IActionResult Post(Movie movie)
+        {
+            if(ModelState.IsValid)
+            {
+                try
+                {
+                   context.Movies.Add(movie);
+                   context.SaveChanges();
+                }
+                catch(Exception e)
+                {
+                    return BadRequest(e.InnerException)
+                }
+            }
+        }
+
     }
 }
