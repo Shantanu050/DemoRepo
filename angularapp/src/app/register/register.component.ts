17ce58b54ac35d9eedc5fdf563cbd8a9f38f99e7
Iamneo <techadmin@iamneo.ai>
2023-11-13 06:45:28 +0000

    Backup Commit

--- angularapp/src/app/register/register.component.ts
+++ angularapp/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'app-register',
@@ -9,10 +10,12 @@ import { NgForm } from '@angular/forms';
 export class RegisterComponent implements OnInit {
 
   constructor() { }
+  fName:string
 RegisterStudent(stdForm:NgForm):void
 {
   console.log(stdForm.value)
   console.log(stdForm.value.firstname)
+  fName=stdForm.value.firstname
 }
   ngOnInit() {
   }
