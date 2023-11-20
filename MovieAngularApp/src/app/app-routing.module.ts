3bdfecfcb36a9c92215df62d39c507d2d493fec0
Iamneo <techadmin@iamneo.ai>
2023-11-20 04:33:34 +0000

    Backup Commit

--- MovieAngularApp/src/app/app-routing.module.ts
+++ MovieAngularApp/src/app/app-routing.module.ts
@@ -25,6 +25,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers:[AuthService]
 })
 export class AppRoutingModule { }
