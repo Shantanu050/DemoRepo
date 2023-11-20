4c60d958432206c6b4eef73380ad27e2d99fe408
Iamneo <techadmin@iamneo.ai>
2023-11-20 04:18:33 +0000

    Backup Commit

--- MovieAngularApp/src/app/app-routing.module.ts
+++ MovieAngularApp/src/app/app-routing.module.ts
@@ -8,15 +8,18 @@ import { DeletemovieComponent } from './deletemovie/deletemovie.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ReactiveformComponent } from './reactiveform/reactiveform.component';
 import { CreatedetailsComponent } from './createdetails/createdetails.component';
+import { AuthGuard } from './guard/auth.guard';
+import { AuthService } from './services/auth.service';
 
 const routes: Routes = [
-  {path:'listmovies',component:ListmovieComponent},
+  {path:'listmovies',component:ListmovieComponent,canActivate:[AuthGuard]},
   {path:'find/:id',component:FindmovieComponent},
   {path:'add',component:AddmovieComponent},
   {path:'edit/:id',component:EditmovieComponent},
   {path:'delete/:id',component:DeletemovieComponent},
   {path:'reactForm',component:ReactiveformComponent},
   {path:'details',component:CreatedetailsComponent}
+
 
 ];
 
