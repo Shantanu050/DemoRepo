d7816197ac9e4de434b56a2cbb2eadfd3a7a3a05
Iamneo <techadmin@iamneo.ai>
2023-11-28 11:11:24 +0000

    Backup Commit

--- jwtapi/Controllers/UserController.cs
+++ jwtapi/Controllers/UserController.cs
@@ -3,29 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using jwtapi.Models;
-using System.Text.RegularExpressions;
-using System.Security.Claims;
-using System.Security.Cryptography;
-using Microsoft.EntityFrameworkCore;
-using System.Collections;
-//-------------------------
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Extensions.Logging;
-using System.Threading.Tasks;
+ 
 using System.Text;
-using Microsoft.EntityFrameworkCore;
 using System.Text.RegularExpressions;
-using Microsoft.AspNetCore.Mvc;
+ 
+using Microsoft.EntityFrameworkCore;
+ 
+using System.IdentityModel.Tokens.Jwt;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.IdentityModel.Tokens.Jwt;
-using System.Security.Cryptography;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using jwtapi.Models;
-using Microsoft.AspNetCore.Authorization;
-
+ 
 namespace jwtapi.Controllers
 {
     [ApiController]
@@ -33,81 +22,96 @@ namespace jwtapi.Controllers
     public class UserController : ControllerBase
     {
         private readonly AppdContext _authContext;
-        public UserController(AppdContext context)
+        public UserController(AppdContext _context)
         {
-            _authContext=context;
+            _authContext=_context;
         }
+ 
         [HttpPost("authenticate")]
-        public async Task<IActionResult> Authenticate([FromBody]User userObj)
+        public async Task<IActionResult> Authenticate([FromBody] User userObj)
         {
-         if(userObj==null) return BadRequest();
-         var user =await _authContext.Users.FirstOrDefaultAsync(x=>x.Username==userObj.Username);
-         if(user==null)
-         {
-            return NotFound(new {Message="User not found"});
-         }
-         user.Token=CreateJwt(user);
-         var newAccessToken=user.Token;
-         var newRefreshToken=CreateRefreshToken();
-         user.RefreshTokenExpiryTime=DateTime.Now.AddDays(2);
-         await _authContext.SaveChangesAsync();
-         return Ok(new TokenApi(){
-            AccessToken=newAccessToken,
-            RefreshToken=newRefreshToken
-         });
+            if(userObj==null)
+            {
+                return BadRequest();
+            }
+            var user=await _authContext.Users.FirstOrDefaultAsync(x=> x.UserName==userObj.UserName);
+            if(user==null)
+            {
+                return NotFound(new{Message="User Not Found"});
+            }
+            user.Token=CreateJwt(user);
+            var newAccessToken=user.Token;
+            var newRefreshToken=CreateRefreshToken();
+            user.RefreshTokenExpiryTime=DateTime.Now.AddDays(2);
+            await _authContext.SaveChangesAsync();
+ 
+ 
+            return Ok(new TokenApi(){
+                AccessToken=newAccessToken,RefreshToken=newRefreshToken
+            });
+ 
         }
+ 
         [HttpPost("register")]
         public async Task<IActionResult> AddUser([FromBody] User userObj)
         {
-            if(userObj==null) return BadRequest();
-            //if any other validation 
-            userObj.Role="user";
-            userObj.Token="";
-            await _authContext.AddAsync(userObj);
-            await _authContext.SaveChanges();
-            return Ok(new {Status=200,Message="User Added"});
-
+            if(userObj==null)
+            {
+                return BadRequest();
+            }  
+        userObj.Role="user";
+        userObj.Token="";
+        await _authContext.Users.AddAsync(userObj);
+        await _authContext.SaveChangesAsync();
+        return Ok(new
+        {
+            Status=200,Meessage="User Added"
         }
+        );
+       
+        }
+ 
         private string CreateJwt(User user)
         {
             var jwtTokenHandler=new JwtSecurityTokenHandler();
-            var key=Encoding.ASCII.GetBytes("LTIM");
+            var key=Encoding.ASCII.GetBytes("ltimindtree...");
             var identity=new ClaimsIdentity(new Claim[]
             {
-               new Claim(ClaimsTypes.Role,user.Role),
-               new Claim(ClaimsTypes.Name,$"{user.Username}")
-
+                new Claim(ClaimTypes.Role,user.Role),
+                new Claim(ClaimTypes.Name,$"{user.UserName}")
             });
+ 
             var credentials=new SigningCredentials(new SymmetricSecurityKey(key),
             SecurityAlgorithms.HmacSha256);
-            var tokenDescriptor=new SecurityTokenDescriptor
-            {
+           
+            var tokenDescriptor=new SecurityTokenDescriptor{
                 Subject=identity,
                 Expires=DateTime.Now.AddDays(2),
                 SigningCredentials=credentials
-
             };
-            var token=jwtTokenHandler.CreateTokent(tokenDescriptor);
+            var token=jwtTokenHandler.CreateToken(tokenDescriptor);
             return jwtTokenHandler.WriteToken(token);
-
         }
+ 
         private string CreateRefreshToken()
         {
             var tokenBytes=RandomNumberGenerator.GetBytes(64);
             var refreshtoken=Convert.ToBase64String(tokenBytes);
-            var tokenUser=_authContext.Users.Any(a=>a.RefreshToken==refreshtoken);
-            if(tokenUser)
+            var tokenInuser=_authContext.Users.Any(a=>a.RefreshToken==refreshtoken);
+ 
+            if(tokenInuser)
             {
                 return CreateRefreshToken();
             }
+           
             return refreshtoken;
         }
+ 
         [HttpGet]
         public async Task<ActionResult<User>> GetAllUsers()
         {
             return Ok(await _authContext.Users.ToListAsync());
         }
-        
+ 
     }
-    
 }
--- jwtapi/Models/User.cs
+++ jwtapi/Models/User.cs
@@ -7,15 +7,15 @@ namespace jwtapi.Models
 {
     public class User
     {
-        public int Id{get;set;}
+  public int Id{get;set;}
         public string? FirstName{get;set;}
-        public string? LastName{get;set;}
-        public string? Username{get;set;}
-        public string? Password{get;set;}
-        public string? Token{get;set;}
-        public string? Role{get;set;}
-        public string? Email{get;set;}
-        public string? RefreshToken{get;set;}
+        public string ?LastName{get;set;}
+        public string ?UserName{get;set;}
+        public string ?Password{get;set;}
+        public string ?Token{get;set;}
+        public string ?Role{get;set;}
+        public string ?Email{get;set;}
+        public string ?RefreshToken{get;set;}
         public DateTime RefreshTokenExpiryTime{get;set;}
     }
     public class TokenApi
