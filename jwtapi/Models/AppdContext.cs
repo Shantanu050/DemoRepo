a85b38a03577d5a1461d6bb0f015a864b0a3a2d6
Iamneo <techadmin@iamneo.ai>
2023-11-28 09:11:25 +0000

    Backup Commit

--- jwtapi/Models/AppdContext.cs
+++ jwtapi/Models/AppdContext.cs
@@ -7,6 +7,8 @@ namespace jwtapi.Models
 {
     public class AppdContext:DbContext
     {
+        public AppdContext(){}
+
         public AppdContext(DbContextOptions<AppdContext>options):base(options)
         {
 
@@ -16,5 +18,7 @@ namespace jwtapi.Models
         {
             builder.Entity<User>().ToTable("users");
         }
+        protected override void OnConfiguring(DbContextOptionsBuilder obj)=>
+        obj.UseSqlServer("User ID=sa;password=examlyMssql@123; server=localhost;Database=AuthDb;trusted_connection=false;Persist Security Info=False;Encrypt=False");
     }
 }
